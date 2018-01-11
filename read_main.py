6cb38c02804bcc3820b4b47941eb4c9145fad9dd
mikedelong <mikedelong@outlook.com>
2018-01-11 11:29:25 -0500

    comment: credit where credit is due on checking presence of column. cleanup: todos should have colons todos: added a to-do item for processing all the XLSX files in the input folder.

--- read_main.py
+++ read_main.py
@@ -13,19 +13,22 @@ logger.addHandler(console_handler)
 console_handler.setLevel(logging_level)
 logger.debug('started')
 
-# todo what if this folder does not exist?
+# todo: what if this folder does not exist?
 input_folder = './input/'
+# todo: have this run in a loop and process all the files
 input_file = 'gridExport_20180109T1540Z.xlsx'
 
 full_input_file = input_folder + input_file
 logger.info('full input file name: %s' % full_input_file)
 df = pd.read_excel(full_input_file, sheetname=0, skiprows=[0, 1, 2, 3, 4])
+
 # https://stackoverflow.com/questions/28538536/deleting-multiple-columns-based-on-column-names-in-pandas
 df = df[df.columns[~df.columns.str.contains('Unnamed:')]]
 logger.info('head after dropping unnamed columns:')
 logger.info(df.head(26))
 
 # convert the Date column, if it exists, to a date
+# https://stackoverflow.com/questions/24870306/how-to-check-if-a-column-exists-in-pandas
 # https://stackoverflow.com/questions/39604094/pandas-delete-all-rows-that-are-not-a-datetime-type
 if 'Date' in df.columns:
     df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
@@ -43,7 +46,7 @@ if 'Date' in df.columns:
 
 
 # write the result to CSV
-# todo what if this folder does not exist?
+# todo: what if this folder does not exist?
 output_folder = './output/'
 output_file = input_file.replace('.xlsx', '.csv')
 logger.info('short output file name: %s' % output_file)
